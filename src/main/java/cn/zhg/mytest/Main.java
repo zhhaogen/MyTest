ee7e4c69bcb873dba7b04cec0a78eb29dec0d954
zhhaogen <zhhaogen@163.com>
2019-03-29 22:47:50 +0800

    发布release

---
+++ src/main/java/cn/zhg/mytest/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 创建于2019-03-29 21:45:02
+ * @author zhhaogen
+ */
+package cn.zhg.mytest;
+
+/**
+ * @author zhhaogen
+ *
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+	}
+
+}
